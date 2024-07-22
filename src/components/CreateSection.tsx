a97f31423733c0533e81fd2712d25b0a378b5bba
Khang Lien <lienchikhang777@gmail.com>
2024-07-22 15:45:13 +0700

    [createGig][mess:: finish basic form, not validate yet, complete add service, delete service]

--- src/components/CreateSection.tsx
+++ src/components/CreateSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react'
 import UploadGigImage from './UploadGigImage'
-import { FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, layout } from '@chakra-ui/react';
+import { FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, Select, Textarea, layout } from '@chakra-ui/react';
 import { Box, Collapse, Divider, List, ListItem, ListItemButton, ListItemText, ListSubheader } from '@mui/material';
 import http from '@/libs/http/http';
 import { Card, Cascader, Form, Radio, TreeSelect, Space, Typography, Button } from 'antd';
@@ -18,6 +18,14 @@ interface Option {
     children?: Option[];
 }
 
+interface IOption {
+    price: number,
+    serviceDesc: string,
+    serviceBenefit: string,
+    deliveryDate: number,
+    serviceLevel: string
+}
+
 
 const CreateSection = () => {
 
@@ -27,16 +35,19 @@ const CreateSection = () => {
     });
     const [types, setTypes] = useState<any[]>([]);
     const [typeValue, setValue] = useState<string>();
-    const [options, setOption] = useState([
+    const [options, setOption] = useState<IOption[]>([
         {
             price: 0,
             serviceDesc: "",
             serviceBenefit: "",
             deliveryDate: 1,
-            serviceLevel: ""
+            serviceLevel: "BASIC"
         },
     ])
     const [form] = Form.useForm();
+    const [isMaximumSer, setMaximum] = useState<boolean>(false);
+    const [level, setLevel] = useState(['BASIC', 'ADVANCED', 'PREMIUM']);
+
 
     const onChange = (newValue: string) => {
         const sub = newValue.split(' ')[1];
@@ -64,7 +75,7 @@ const CreateSection = () => {
         }
     };
 
-    console.log({ input, typeValue, options });
+    console.log({ options });
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setInput({ ...input, [e.currentTarget.name]: e.currentTarget.value })
 
@@ -95,7 +106,7 @@ const CreateSection = () => {
                 <div className="gigForm">
                     <FormControl isInvalid={isErrorGigDesc}>
                         <FormLabel>Gig Description</FormLabel>
-                        <Input type='text' name='jobDesc' value={input.jobDesc} onChange={handleInputChange} />
+                        <Textarea name='jobDesc' value={input.jobDesc} onChange={(e) => setInput({ ...input, [e.currentTarget.name]: e.currentTarget.value })} />
                         {!isErrorGigDesc ? (
                             <FormHelperText>
                                 Enter the email you'd like to receive the newsletter on.
@@ -147,98 +158,181 @@ const CreateSection = () => {
                     initialValues={{ items: [{}] }}
                 >
                     <Form.List name="items">
-                        {(fields, { add, remove }) => (
-                            <div style={{ display: 'flex', rowGap: 16, flexDirection: 'column' }}>
-                                {fields.map((field) => (
-                                    <Card
+                        {(fields, { add, remove }) => {
+                            console.log({ fields });
+                            return <div style={{ display: 'flex', rowGap: 16, flexDirection: 'column' }}>
+                                {fields.map((field) => {
+                                    return <Card
                                         size="small"
                                         title={`Service ${field.name + 1}`}
                                         key={field.key}
                                         extra={
                                             <CloseOutlined
                                                 onClick={() => {
+                                                    console.log('key', field.name);
+                                                    //delete option
+                                                    // const curOps  = [...options];
+                                                    // let deleted = options[field.key];
+                                                    options.splice(field.name, 1);
+
+                                                    //update service level
+                                                    const newOtps = options.map((opt, idx) => {
+                                                        return {
+                                                            ...opt,
+                                                            serviceLevel: idx == 0 ? 'BASIC' : idx == 1 ? 'ADVANCE' : 'PREMIUM'
+                                                        }
+                                                    });
+
+                                                    console.log('after map', newOtps);
+                                                    setOption([...newOtps]);
                                                     remove(field.name);
+
                                                 }}
                                             />
                                         }
                                     >
                                         <Form.Item label="Service Description" name={[field.name, 'serviceDesc']}>
-                                            <FormControl isInvalid={isErrorGigDesc}>
-                                                <Input type='text' name='serviceDesc' value={options[field.key].serviceDesc} onChange={(e) => {
+                                            <FormControl isInvalid={options[field.name]?.serviceDesc === ''}>
+                                                <Textarea name='serviceDesc' value={options[field.name]?.serviceDesc} onChange={(e) => {
                                                     const updatedOptions = [...options];
-                                                    updatedOptions[field.key] = {
-                                                        ...updatedOptions[field.key],
+                                                    updatedOptions[field.name] = {
+                                                        ...updatedOptions[field.name],
                                                         [e.currentTarget.name]: e.currentTarget.value
                                                     };
                                                     setOption(updatedOptions);
                                                 }} />
-                                                {!isErrorGigDesc ? (
+                                                {!options[field.name]?.serviceDesc ? (
                                                     <FormHelperText>
-                                                        Enter the email you'd like to receive the newsletter on.
+                                                        Enter service description.
                                                     </FormHelperText>
                                                 ) : (
-                                                    <FormErrorMessage>Email is required.</FormErrorMessage>
+                                                    <FormErrorMessage>Service description is required.</FormErrorMessage>
                                                 )}
                                             </FormControl>
                                         </Form.Item>
 
                                         <Form.Item label="Service price" name={[field.name, 'price']}>
-                                            <FormControl isInvalid={isErrorGigDesc}>
-                                                <Input type='number' name='price' value={options[field.key].price} onChange={(e) => {
+                                            <FormControl isInvalid={options[field.name]?.price === 0 || options[field.name]?.price <= 0}>
+                                                <Input type='number' name='price' value={options[field.name]?.price} onChange={(e) => {
                                                     const updatedOptions = [...options];
-                                                    updatedOptions[field.key] = {
-                                                        ...updatedOptions[field.key],
+                                                    updatedOptions[field.name] = {
+                                                        ...updatedOptions[field.name],
                                                         [e.currentTarget.name]: e.currentTarget.value
                                                     };
                                                     setOption(updatedOptions);
                                                 }} />
-                                                {!isErrorGigDesc ? (
+                                                {!options[field.name]?.price || options[field.name]?.price > 0 ? (
                                                     <FormHelperText>
-                                                        Enter the email you'd like to receive the newsletter on.
+                                                        Enter service price you'd like to receive.
                                                     </FormHelperText>
                                                 ) : (
-                                                    <FormErrorMessage>Email is required.</FormErrorMessage>
+                                                    <FormErrorMessage>Service price is required.</FormErrorMessage>
                                                 )}
                                             </FormControl>
                                         </Form.Item>
 
                                         <Form.Item label="Service benefit" name={[field.name, 'serviceBenefit']}>
-                                            <FormControl isInvalid={isErrorGigDesc}>
-                                                <Input type='text' name='serviceBenefit' value={options[field.key].serviceBenefit} onChange={(e) => {
+                                            <FormControl isInvalid={options[field.name]?.serviceBenefit === ''}>
+                                                <Textarea name='serviceBenefit' value={options[field.name]?.serviceBenefit} onChange={(e) => {
                                                     const updatedOptions = [...options];
-                                                    updatedOptions[field.key] = {
-                                                        ...updatedOptions[field.key],
+                                                    updatedOptions[field.name] = {
+                                                        ...updatedOptions[field.name],
                                                         [e.currentTarget.name]: e.currentTarget.value
                                                     };
                                                     setOption(updatedOptions);
                                                 }} />
-                                                {!isErrorGigDesc ? (
+                                                {!options[field.name]?.serviceBenefit ? (
                                                     <FormHelperText>
-                                                        Enter the email you'd like to receive the newsletter on.
+                                                        Enter service benefit you'd like to give.
                                                     </FormHelperText>
                                                 ) : (
-                                                    <FormErrorMessage>Email is required.</FormErrorMessage>
+                                                    <FormErrorMessage>Service benefit is required.</FormErrorMessage>
                                                 )}
                                             </FormControl>
                                         </Form.Item>
-                                    </Card>
-                                ))}
 
-                                <Button type="dashed" onClick={() => {
-                                    if (fields.length === 3) return;
+                                        <Form.Item label="Service level" name={[field.name, 'serviceLevel']}>
+                                            <FormControl isInvalid={options[field.name]?.serviceLevel === ''}>
+                                                <Select name='serviceLevel' placeholder={
+                                                    (field.name == 0) ? 'BASIC' : (field.name == 1) ? 'ADVANCED' : 'PREMIUM'
+                                                } value={
+                                                    (field.name == 0) ? 'BASIC' : (field.name == 1) ? 'ADVANCED' : 'PREMIUM'
+                                                }
+                                                //  onChange={(e) => {
+                                                //     const updatedOptions = [...options];
+                                                //     updatedOptions[field.name] = {
+                                                //         ...updatedOptions[field.name],
+                                                //         [e.currentTarget.name]: e.currentTarget.value
+                                                //     };
+                                                //     setOption(updatedOptions);
+                                                // }} 
+                                                >
+                                                    {/* {(field.name == 0) && <option value={'BASIC'}>BASIC</option>}
+                                                {(field.name == 1) && <option value={'ADVANCED'}>ADVANCED</option>}
+                                                {(field.name == 2) && <option value={'PREMIUM'}>PREMIUM</option>} */}
+
+                                                </Select>
+                                                {!options[field.name]?.serviceLevel ? (
+                                                    <FormHelperText>
+                                                        Enter delivery date.
+                                                    </FormHelperText>
+                                                ) : (
+                                                    <FormErrorMessage>Delivery date is required.</FormErrorMessage>
+                                                )}
+                                            </FormControl>
+                                        </Form.Item>
+
+                                        <Form.Item label="Delivery date" name={[field.name, 'deliveryDate']}>
+                                            <FormControl isInvalid={options[field.name]?.deliveryDate === 0}>
+
+                                                <Select name='deliveryDate' placeholder='Select date' onChange={(e) => {
+                                                    const updatedOptions = [...options];
+                                                    updatedOptions[field.name] = {
+                                                        ...updatedOptions[field.name],
+                                                        [e.currentTarget.name]: e.currentTarget.value
+                                                    };
+                                                    setOption(updatedOptions);
+                                                }} >
+                                                    <option value='1'>Express 24H</option>
+                                                    <option value='3'>Up to 3 days</option>
+                                                    <option value='7'>Up to 7 days</option>
+                                                    <option value=''>Anytime</option>
+                                                </Select>
+                                                {!options[field.name]?.deliveryDate ? (
+                                                    <FormHelperText>
+                                                        Enter delivery date.
+                                                    </FormHelperText>
+                                                ) : (
+                                                    <FormErrorMessage>Delivery date is required.</FormErrorMessage>
+                                                )}
+                                            </FormControl>
+                                        </Form.Item>
+
+
+                                    </Card>
+                                })}
+
+                                <Button type="dashed" disabled={fields.length === 3} onClick={() => {
+                                    console.log('btn click', fields.length);
+                                    if (fields.length + 1 === 3) {
+                                        setMaximum(true);
+                                    }
+                                    if (fields.length === 3) {
+                                        return;
+                                    }
                                     add();
                                     setOption([...options, {
                                         price: 0,
                                         serviceDesc: "",
                                         serviceBenefit: "",
                                         deliveryDate: 1,
-                                        serviceLevel: ""
+                                        serviceLevel: (fields.length + 1 == 1) ? 'BASIC' : (fields.length + 1 == 2) ? 'ADVANCED' : 'PREMIUM'
                                     },])
                                 }} block>
                                     + Add Item
                                 </Button>
                             </div>
-                        )}
+                        }}
                     </Form.List>
                 </Form>
             </div>
